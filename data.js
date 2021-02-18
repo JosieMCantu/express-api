48c629fc9bf0acb631369039581c120118c2cb24
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-02-18 15:56:40 -0800

    passes tests

---
+++ data.js
@@ -0,0 +1,47 @@
+const quotesCreek = [
+    {
+        image: 1940,
+        id: 1,
+        name: 'David',
+        price: 65,
+        quote: 'red',
+    },
+    {
+        image: 1930,
+        id: 2,
+        name: 'Alexis',
+        price: 100,
+        color: 'wood',
+    },
+    {
+        image: 1950,
+        id: 3,
+        name: 'Moira',
+        price: 165,
+        color: 'silver',
+    },
+    {
+        image: 1910,
+        id: 4,
+        name: 'Stevie',
+        price: 25,
+        color: 'gold',
+    },
+    {
+        image: 1910,
+        id: 5,
+        name: 'Roland',
+        price: 25,
+        color: 'gold',
+    },
+    {
+        image: 1910,
+        id: 6,
+        name: 'Johnny',
+        price: 25,
+        color: 'gold',
+    }
+];
+module.exports = {
+    quotesCreek
+};
